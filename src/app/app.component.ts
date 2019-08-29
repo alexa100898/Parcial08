31e156ef01bda6696b97d43d2cbd3dcfc318ca22
Salas Didacticas Sede Bogota <juand-escobart@unilibre.edu.co>
2019-08-28 21:52:32 -0500

    otro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,12 @@ nombre:string = '';
 distancia:string = '';
 lunas:string = '';
 masa:string = '';
+anillos:string = '';
   planetas:any = [
-    {nombre: 'Tierra', distancia: '5m', lunas: '1', masa: '25kg'},
-    {nombre: 'Marte', distancia: '15m', lunas: '2', masa: '75kg'},
-    {nombre: 'Jupiter', distancia: '25m', lunas: '1', masa: '125kg'},
-    {nombre: 'Saturno', distancia: '35m', lunas: '2', masa: '15kg'},
-    {nombre: 'Pluton', distancia: '45m', lunas: '1', masa: '5kg'}
+    {nombre: 'Tierra', distancia: '5m', lunas: '1', masa: '25kg', anillos: 1, tipo:1},
+    {nombre: 'Marte', distancia: '15m', lunas: '2', masa: '75kg', anillos: 2, tipo:2},
+    {nombre: 'Jupiter', distancia: '25m', lunas: '1', masa: '125kg', anillos: 1, tipo:1},
+    {nombre: 'Saturno', distancia: '35m', lunas: '2', masa: '15kg', anillos: 2, tipo:2},
+    {nombre: 'Pluton', distancia: '45m', lunas: '1', masa: '5kg', anillos: 1, tipo:1}
   ]
 }
