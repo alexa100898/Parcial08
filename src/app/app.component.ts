98eae93ebd974154ff308df225c3cb328a9cebce
Salas Didacticas Sede Bogota <juand-escobart@unilibre.edu.co>
2019-08-28 21:27:44 -0500

    otros cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Parcial2808';
+nombre:string = '';
+distancia:string = '';
+lunas:string = '';
+masa:string = '';
+  planetas:any = [
+    {nombre: 'Tierra', distancia: '5m', lunas: '1', masa: '25kg'},
+    {nombre: 'Marte', distancia: '15m', lunas: '2', masa: '75kg'},
+    {nombre: 'Jupiter', distancia: '25m', lunas: '1', masa: '125kg'},
+    {nombre: 'Saturno', distancia: '35m', lunas: '2', masa: '15kg'},
+    {nombre: 'Pluton', distancia: '45m', lunas: '1', masa: '5kg'}
+  ]
 }
